56b4702b3612676da34623c4d76b08b8cbb64007
Chanithi Perera <chanithidehansa@gmail.com>
2026-04-18 15:17:57 +0530

    Update AuthStack.js

--- frontend/src/navigation/AuthStack.js
+++ frontend/src/navigation/AuthStack.js
@@ -4,6 +4,7 @@ import HomeScreen from '../screens/home/HomeScreen';
 import LoginScreen from '../screens/auth/LoginScreen';
 import RegisterScreen from '../screens/auth/RegisterScreen';
 import TicketShowPlaceholder from '../screens/ticketShow/TicketShowPlaceholder';
+import TicketBookingScreen from '../screens/ticketShow/TicketBookingScreen';
 import EventsPlaceholder from '../screens/events/EventsPlaceholder';
 import FeedbackPlaceholder from '../screens/feedback/FeedbackPlaceholder';
 import AnimalsPlaceholder from '../screens/animals/AnimalsPlaceholder';
@@ -20,6 +21,7 @@ export default function AuthStack() {
       <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Sign in' }} />
       <Stack.Screen name="Register" component={RegisterScreen} options={{ title: 'Create account' }} />
       <Stack.Screen name="TicketShow" component={TicketShowPlaceholder} options={{ title: 'Entry Tickets and Show Booking' }} />
+      <Stack.Screen name="TicketBooking" component={TicketBookingScreen} options={{ title: 'Booking' }} />
       <Stack.Screen name="Events" component={EventsPlaceholder} options={{ title: 'Event Booking' }} />
       <Stack.Screen name="Feedback" component={FeedbackPlaceholder} options={{ title: 'Submit Feedbacks, Inquiries or Reviews' }} />
       <Stack.Screen name="Animals" component={AnimalsPlaceholder} options={{ title: 'Animal Information and Education' }} />
