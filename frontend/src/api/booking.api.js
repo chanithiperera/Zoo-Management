83ee65d2b24db358bff2e8c3629b67a89018fcfd
Chanithi Perera <chanithidehansa@gmail.com>
2026-04-23 18:23:54 +0530

    Create booking.api.js

---
+++ frontend/src/api/booking.api.js
@@ -0,0 +1,21 @@
+import apiClient from './client';
+
+export async function getTicketCatalog() {
+  const res = await apiClient.get('/ticket-show/catalog');
+  return res.data;
+}
+
+export async function createBooking(payload) {
+  const res = await apiClient.post('/ticket-show/bookings', payload);
+  return res.data;
+}
+
+export async function getMyBookings() {
+  const res = await apiClient.get('/ticket-show/bookings/me');
+  return res.data;
+}
+
+export async function getMyBooking(id) {
+  const res = await apiClient.get(`/ticket-show/bookings/${id}`);
+  return res.data;
+}
